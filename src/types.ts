b05f797512614fd69c85c15fb46a788087adc6bc
Tiago Marques <legendary.gates@gmail.com>
2018-08-16 16:31:55 +0100

    Updated code

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,14 @@
 
+import { Source } from 'webpack-sources';
+
+export interface AssetsObject {
+  [key: string]: Source;
+}
+
 export interface ManifestIcon {
   src: string;
   type: string;
   sizes?: string | string[];
-}
-
-export interface ConfigurationInput {
-  path: string;
-  pretty: boolean;
-  name: string;
-  description: string;
-  lang: string;
-  short_name?: string;
-  icons?: string | string[];
-  start_url?: string;
-  scope?: string;
-  dir?: string;
-  orientation?: string;
-  display?: string;
-  background_color?: string;
-  theme_color?: string;
 }
 
 export interface Configuration {
